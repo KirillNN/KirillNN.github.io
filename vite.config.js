24670dbd878fe0e8acae3227c4cd37c12303922a
Kirill <wmforum@bk.ru>
2024-10-27 19:05:04 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://KirillNN.github.io/'
+  base: 'https://KirillNN.github.io/KirillNN.github.io'
 })
